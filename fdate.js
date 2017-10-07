60c9a3ae03218dcbae610e913aaadb6b4719477c
@Quasic <qbasic010@aim.com>
2017-10-07 16:09:56 -0400

    generalize

--- fdate.js
+++ fdate.js
@@ -18,6 +18,12 @@ fdate.MN=["January","February","March","April","May","June","July","August","Sep
 fdate.loaded=new Date();
 fdate.now=function(){return new Date;};//could be overridden
 fdate.consolentero=typeof console!="undefined"&&console.trace&&console.entero;
+try{
+arguments.callee;
+fdate.isStrict=true;
+}catch(e){
+fdate.isStrict=false;
+}
 fdate.entero=fdate.consolentero||function(){return function(o){return o;};};
 fdate.numDays=function(m,y){return fdate.entero(0,"fdate.numDays",[m,y])(m==1?new Date(new Date("March 1, "+y+" 0:00")-9999).getDate():30+[1,0,1,0,1,0,1,1,0,1,0,1][m]);};
 fdate.relative=function(D,k){var d=new Date-D,m=Math.abs(d),i;for(i=0;i<fdate.relative.a.length;i++)if(m>=fdate.relative.a[i][0])return(m=Math.round(m/fdate.relative.a[i][0]))+fdate.relative.a[i][k||1]+(k===3&&m===1?'':'s')+(d<0?' from now':' ago');};
@@ -29,26 +35,27 @@ fdate.relative.a=[
 [1000,"s"," sec"," second"],
 [0,"ms"," ms"," millisecond"]
 ];//{ms:1,s:1000,m:60000,h:3600000};
-fdate.getFormatter=function(f,utc){var x=fdate.entero(0,"fdate.getFormatter",arguments),a=[],c,n,i,t,r=arguments.callee.r,R=0,rR=0;
+(function(){var
+F=fdate.getFormatter=function(f,utc){var x=fdate.entero(0,"fdate.getFormatter",arguments),a=[],c,n,i,t,r=F.r,R=0,rR=0;
 for(i=0;i<f.length;i+=n){
 c=f.charAt(i);
 for(n=1;i+n<f.length&&f.charAt(i+n)===c;n++);
 c=f.substring(i,i+n);
 if(c==='x'){
-rR=R=arguments.callee.fi[f.charAt(i+n)];
+rR=R=F.fi[f.charAt(i+n)];
 R="(R."+R[0]+"===D."+R[0]+")&&Math.abs(D-R)<"+R[1];
 }else{
-if(utc&&arguments.callee.f["UTC"+c])c="UTC"+c;
-if(arguments.callee.f[c]){
-t=utc&&c.substring(0,2)!="UTC"?fdate.getFormatter.f[c].replace(r,"D.getUTC"):fdate.getFormatter.f[c];
+if(utc&&F.f["UTC"+c])c="UTC"+c;
+if(F.f[c]){
+t=utc&&c.substring(0,2)!="UTC"?F.f[c].replace(r,"D.getUTC"):F.f[c];
 a.push(R?"("+R+"?'':"+t+")":t);
 }else if((t=a[a.length-1]).charAt(0)==="'"&&t.substring(t.length-1)==="'")a[a.length-1]=t.substring(0,t.length-1)+c+"'";else a.push("'"+c+"'");
 R=0;
 }}
-c=new Function("o","var "+(fdate.consolentero?"x=console.entero('fdate.getFormatter(\""+f.replace(/"/g,'\\"')+"\","+utc+")',arguments.callee,arguments),":"")+"D=typeof o!=='undefined'?new Date(o):new Date"+(rR?",R=new Date":"")+";return "+(fdate.consolentero?"x(":"")+"isNaN(D)?'Invalid date: '+o:"+a.join("+")+(fdate.consolentero?")":"")+";");
+c=new Function("o","var "+(fdate.consolentero?"x=console.entero('fdate.getFormatter(\""+f.replace(/"/g,'\\"')+"\","+utc+")',arguments.callee,arguments),":"")+"D=typeof o==='undefined'?new Date:o instanceof Date?o:new Date(o)"+(rR?",R=new Date":"")+";return "+(fdate.consolentero?"x(":"")+"isNaN(D)?'Invalid date: '+o:"+a.join("+")+(fdate.consolentero?")":"")+";");
 x();return c;};
-fdate.getFormatter.r=/D\.get/g;
-fdate.getFormatter.fi={
+F.r=/D\.get/g;
+F.fi={
 Y:["getFullYear()",366*24*3600000],
 M:["getMonth()",31*24*3600000],
 W:["getDay()",24*3600000],
@@ -63,7 +70,7 @@ s:["getSeconds()",1000],
 u:["getMilliseconds()",1],
 z:["getTimezoneOffset()",Infinity]
 };
-fdate.getFormatter.f={
+F.f={
 Y:"D.getFullYear()",
 YY:"(D.getFullYear()+'').substring(2)",
 YYYY:"D.getFullYear()",
@@ -96,13 +103,13 @@ s:"D.getSeconds()",
 ss:"(D.getSeconds()+100+'').substring(1)",
 //D.getMilliseconds()===D%1000, but easier to convert to UTC with function
 u:"(D.getMilliseconds()+1000+'').charAt(1)",
-uu:"(D.getMilliseconds()+1000+'').substring(1,2)",
-uuu:"(D.getMilliseconds()+1000+'').substring(1,3)",
+uu:"(D.getMilliseconds()+1000+'').substring(1,3)",
+uuu:"(D.getMilliseconds()+1000+'').substring(1,4)",
 z:"'UTC'+(D.getTimezoneOffset()<0?'':'+')+(Math.floor(D.getTimezoneOffset()/60)+100+'').substring(1)+(D.getTimezoneOffset()%60+10000+'').substring(3)",//no getUTCTimezoneOffset, duh
 UTCz:"'UTC+0000'",
 zz:"fdate.tzr.exec(D.toString())[1]",
 UTCzz:"'(UTC)'"
 };
+})();
 fdate.tzr=/\(([^)]+)\)/;
 if(typeof module!=="undefined"&&module.exports)module.exports=fdate;
-fdate;

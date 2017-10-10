c26fa4b88ac15b819bc80dacf5bcdbf12c68804c
Quasic <qbasic010@aim.com>
2017-10-10 16:21:20 -0400

    function-level "use strict" prefered, drop callee and caller altogether

--- shim/console.entero.js
+++ shim/console.entero.js
@@ -27,41 +27,38 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
-*///"use strict"; doesn't work with arguments.callee
+*/
 if(typeof console!=="object")var console={log:function(){},error:function(){}};
-(function(){var
+(function(){"use strict";var
 OptS=Object.prototype.toString,
 stringFrom=typeof stringFrom==="function"||OptS.apply(stringFrom)==="[object Function]"?stringFrom:function(o){var t=typeof o;return t==="object"?OptS.apply(o):"["+t+" "+o+"]";},
 S=0,
 L="";
-function functionDescriptor(f){var s=f.toString(),i=s.indexOf(')');return i<2?s:s.substring(0,i+1);}
-function fn(n,params,d){var
+function fn(object,n,params,d){var
 p=[],
 i;
 if(params&&params.length)for(i=0;i<params.length;i++)p[i]=params[i];//toSource?
-return(d.object?stringFrom(d.object)+'.':'')
+return(object?stringFrom(object)+'.':'')
 +n+(p.length?'('+p.join(',')+')':'')
 +(d.deprecated?'[deprecated: '+d.deprecated+']':'')
-+(params&&params.callee?" as "+functionDescriptor(params.callee):'')
-//caller empty outside of function and doesn't work with strict mode
-}
-console.entero=function(n,params,O){var
+;}
+console.entero=function(object,n,params,O){"use strict";var
 d=O||{},
-N=fn(n,params,d),
+N=fn(object,n,params,d),
 x=S,
-f=++S+": "+N+" entered.";
+f="$"+ ++S+": "+N+" entered.";
 console.log(f);
-L+=f;
+L+=f+"\n";
 f=function(r){var
-s=S+": "+N+" returned "+r;
+s="$"+S+": "+N+" returned "+r;
 console.log(s);
-L+=s;
+L+=s+"\n";
 S=x;
 return r;};
 f.resume=function(e,O){var
-s=S+": "+(O?fn(n,params,O):N)+"Error: "+e.message;
+s="$"+S+": "+(O?fn(object,n,params,O):N)+"Error: "+e.message;
 console.error(s);
-L+=s;
+L+=s+"\n";
 S=x+1;
 return e;
 };
